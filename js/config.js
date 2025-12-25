8a734fb49c1958b7caa977801faec4dc658dd66d
cargolyze <iampriyanshubhardwaj@gmail.com>
2025-12-25 18:27:50 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,4 @@
 const CONFIG = {
     SUPABASE_URL: 'https://ijxchpbubzomytxouhqw.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqeGNocGJ1YnpvbXl0eG91aHF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NDk0NzUsImV4cCI6MjA4MTUyNTQ3NX0.Ro6zlvBrKRaczXJ6t2kF6_U7Kc3sYN56K115nBaNuwI',
-    RESEND_API_KEY: 're_SEybofoE_8sht4edMdS7u6Ka2frmRLxGk'
 };
